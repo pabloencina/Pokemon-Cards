b14ec0b570ebdff7b31eaeb4fd20843a5ec98005
Pablo <encina.pd@gmail.com>
2022-07-05 17:47:44 -0300

    pop-up del boton power

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 let contenedor = document.getElementById("contenedor");
 let btnSiguiente = document.getElementById("btnSiguiente");
 let btnAnterior = document.getElementById("btnAnterior");
+let inputBusqueda = document.getElementById("inputBusqueda");
+let btnBusqueda = document.getElementById("btnBusqueda");
 
 const urlPokemon = "https://pokeapi.co/api/v2/pokemon/";
+
 const getPokemon = async function (id) {
   try {
     let resultado = await fetch(`${urlPokemon}${id}/`);
-    let resultadoJson = await resultado.json();
-    crearPokemon(resultadoJson);
+    if (resultado.status === 200) {
+      let resultadoJson = await resultado.json();
+      console.log(resultadoJson.name)
+      crearPokemon(resultadoJson);
+      //await getPokemonPower(resultadoJson)
+    } else {
+      mostrarPokemonInexistente();
+    }
   } catch (error) {
     console.log(error);
   }
 };
-/*
+
 const urlPokemonEfecto = "https://pokeapi.co/api/v2/ability/";
 const getPokemonPower = async function (id) {
   try {
-    let resultado = await fetch(`${urlPokemonEfecto}${id}/`);
-    let resultadoJson = await resultado.json();
-    //await crearPokemon(resultadoJson);
-
-    //await crearCardDePower(resultadoJson);
+    let result = await fetch(`${urlPokemonEfecto}${id}/`);
+    let resultJson = await result.json();
+    console.log(resultJson);
   } catch (error) {
     console.log(error);
   }
-};*/
+};
 
 function crearPokemon(pokemon) {
-  console.log(pokemon.id);
   contenedor.innerHTML += `
     <div class="row" id="cardContenedor">
       <div class="col"></div>
@@ -36,13 +42,11 @@ function crearPokemon(pokemon) {
           pokemon.sprites.front_default
         }" class="card-img-top" alt="..." />
         <div class="card-body">
-          <h1 class="card-title">${pokemonNombre(pokemon)}</h1>
+          <h1 } class="card-title">${pokemonNombre(pokemon)}</h1>
           <p class="card-text">
             #${pokemon.id.toString().padStart(3, 0)}
           </p>
-          <button id="btnPower-${
-            pokemon.id
-          }" class="btn btn-primary btn-power" type="button">Power</button>
+          <button id="btnPower-${pokemon.id}" class="btn btn-primary btn-power" type="button" data-pokemon-id=${pokemon.id} data-pokemon-name=${pokemonNombre(pokemon)}>Power</button>
         </div>
         </div>
     </div>
@@ -65,9 +69,11 @@ async function traerSiguientesPokemons(offset, limit) {
   for (let i = offset; i < offset + limit; i++) {
     await getPokemon(i);
   }
-  document.querySelectorAll(".btn-power").forEach((item) => {
-    item.addEventListener("click", () => {
-      console.log("holii");
+  document.querySelectorAll(".btn-power").forEach( (item) => {
+    item.addEventListener("click",async () => {
+      console.log(item.dataset.pokemonId);
+      swal ( item.dataset.pokemonName , item.dataset.pokemonName);
+      await getPokemonPower(item.dataset.pokemonId);
     });
   });
 }
@@ -87,3 +93,28 @@ btnAnterior.addEventListener("click", async () => {
     await traerSiguientesPokemons(offset, limit);
   }
 });
+
+btnBusqueda.addEventListener("click", () => {
+  if (inputBusqueda.value = "") {
+    crearPokemon(inputBusqueda.value);
+    
+    console.log("chau");
+  } else {
+    mostrarInputVacio();
+  }
+  inputBusqueda.value = "";
+});
+
+function mostrarInputVacio() {
+  Toastify({
+    text: "ingrese un Pokemon",
+    duration: 6000,
+  }).showToast();
+}
+
+function mostrarPokemonInexistente() {
+  Toastify({
+    text: "Pokemon no encontrado",
+    duration: 6000,
+  }).showToast();
+}
